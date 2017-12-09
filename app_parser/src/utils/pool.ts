931120a1e2fe79662f35a3f27e13c1e663939adc
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2017-12-09 18:54:07 -0500

    fix(util/pool): IntelliJ was highlighting some errors in arrow fn

--- app_parser/src/utils/pool.ts
+++ app_parser/src/utils/pool.ts
@@ -4,7 +4,7 @@ import { Pool } from 'generic-pool'
  * Promisified auto closed pool
  */
 export const createAutoClosedPool = <T>(pool: Pool<T>) => {
-  return async <K>(fn: (resource: T) => Promise<K>): Promise<K> => {
+  return async function <K>(fn: (resource: T) => Promise<K>): Promise<K> {
     const resource = await pool.acquire()
     try {
       return await fn(resource)
