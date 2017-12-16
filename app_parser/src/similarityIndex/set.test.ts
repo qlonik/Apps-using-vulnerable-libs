0a466b20196463c2e9c8a5a724d449ff4af1522f
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2017-12-16 00:24:43 -0500

    feat(set.test): add test file to ensure index fns() are correct

---
+++ app_parser/src/similarityIndex/set.test.ts
@@ -0,0 +1,71 @@
+import { test } from 'ava'
+import {
+  difference, intersection, isSuperset, jaccardIndex, jaccardLikeForSortedArr,
+  similarityIndexToLib, union
+} from './set'
+
+
+test('isSuperset', t => {
+  t.true(isSuperset(new Set([1, 2, 3]), new Set([1, 2, 3, 4, 5])))
+  t.false(isSuperset(new Set([1, 2, 3]), new Set([2, 3, 4])))
+})
+
+test('intersection', t => {
+  t.deepEqual(new Set([2, 3, 4]), intersection(new Set([1, 2, 3, 4]), new Set([2, 3, 4, 5])))
+})
+
+test('union', t => {
+  t.deepEqual(new Set([1, 2, 3, 4, 5]), union(new Set([1, 2, 3, 4]), new Set([3, 4, 5])))
+})
+
+test('difference', t => {
+  t.deepEqual(new Set([1, 2]), difference(new Set([1, 2, 3, 4]), new Set([3, 4, 5])))
+})
+
+test('jaccard index', t => {
+  const a = new Set([1, 2, 3])
+  const b = new Set([2, 3, 4])
+  const ex = { val: 0.5, num: 2, den: 4 }
+
+  t.deepEqual(ex, jaccardIndex(a, b))
+})
+
+test('jaccard index with repeating', t => {
+  const a = new Set([1, 2, 2, 3, 3])
+  const b = new Set([2, 2, 3, 4])
+  const ex = { val: 0.5, num: 2, den: 4 }
+
+  t.deepEqual(ex, jaccardIndex(a, b))
+})
+
+test('similarity index to lib', t => {
+  const a = new Set([1, 2, 3])
+  const b = new Set([2, 3, 4, 5])
+  const ex = { val: 0.5, num: 2, den: 4 }
+
+  t.deepEqual(ex, similarityIndexToLib(b, a))
+})
+
+test('similarity index to lib with repeating', t => {
+  const a = new Set([1, 2, 2, 3, 3])
+  const b = new Set([2, 2, 3, 4, 5])
+  const ex = { val: 0.5, num: 2, den: 4 }
+
+  t.deepEqual(ex, similarityIndexToLib(b, a))
+})
+
+test('jaccard like for sorted arrays', t => {
+  const a = [1, 2, 3]
+  const b = [2, 3, 4]
+  const ex = { val: 0.5, num: 2, den: 4 }
+
+  t.deepEqual(ex, jaccardLikeForSortedArr(a, b))
+})
+
+test('jaccard like for sorted arrays with repeating', t => {
+  const a = [1, 2, 2, 3, 3]
+  const b = [2, 2, 3, 4]
+  const ex = { val: 0.5, num: 3, den: 6 }
+
+  t.deepEqual(ex, jaccardLikeForSortedArr(a, b))
+})
