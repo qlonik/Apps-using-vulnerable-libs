7ae49b318e4e690ea2fff68b67afd90c10a75327
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2017-12-17 03:02:45 -0500

    feat(reanalyser): add module to reanalyse all libraries

---
+++ app_parser/src/dumpWatch/reanalyser.ts
@@ -0,0 +1,96 @@
+import { oneLine } from 'common-tags'
+import { getNamesVersions } from '../parseLibraries'
+import { onelineUtilInspect, stdoutLog } from '../utils/logger'
+import {
+  clientMessage,
+  clientMessageType,
+  LOG_NAMESPACE,
+  messageFrom,
+  reanalysisResult,
+  serverMessage,
+  serverMessageType
+} from './common'
+import { ChildProcessWithLog, createAutoClosedPool, workerPool } from './workerPool'
+
+
+const LIB_PATH = '../data/sample_libs'
+
+const log = stdoutLog(LOG_NAMESPACE)
+const useExecutorsPool = createAutoClosedPool(workerPool)
+
+const reanalyseLibs = (
+  { libsPath, name, version }: {
+    libsPath: string,
+    name: string,
+    version: string,
+  }) => {
+
+  return async (worker: ChildProcessWithLog) => {
+
+    log('(w:%o) got %o', worker.pid, `${name}@${version}`)
+
+    worker.send(<serverMessage>{
+      from: messageFrom.server,
+      type: serverMessageType.reanalyseLib,
+      libsPath,
+      name,
+      version,
+    })
+
+    const { name: nameBack, version: versionBack, analysis } =
+      await new Promise<reanalysisResult>(((resolve, reject) => {
+        worker.once('message', (msg: clientMessage) => {
+          if (msg.type === clientMessageType.reanalysisResult) {
+            resolve({ name: msg.name, version: msg.version, analysis: msg.analysis })
+          }
+          else {
+            reject(new Error('wrong message type received'))
+          }
+        })
+      }))
+
+    if (!analysis) {
+      log(oneLine`
+        (w:%o)
+        No analysis files produced for lib %o
+      `, worker.pid, `${name}@${version}`)
+    }
+    else {
+      const nvSame = nameBack === name && versionBack === version
+      const args = (<any[]>[])
+        .concat(worker.pid, `${name}@${version}`)
+        .concat(nvSame ? [] : `${nameBack}@${versionBack}`)
+        .concat(onelineUtilInspect({ analysis }))
+
+      log(oneLine`
+        (w:%o) fin %o
+        ${nvSame ? '' : '(recv %o)'}
+        ${analysis.length ? '' : '(no analysis files produced)'}
+        %s
+      `, ...args)
+    }
+
+    return analysis
+  }
+}
+
+async function main() {
+  log('started')
+  const nameVersions = await getNamesVersions(LIB_PATH)
+  const analysisPromises = nameVersions.map(({ name, version }) => {
+    return useExecutorsPool(reanalyseLibs({ libsPath: LIB_PATH, name, version }))
+  })
+  const analysis = await Promise.all(analysisPromises)
+
+  await workerPool.drain()
+  await workerPool.clear()
+  log('worker pool closed')
+
+  return analysis
+}
+
+
+main()
+  .then(() => log('Everything is done!'))
+  .catch((err) => log(`Some global error:\n${err}\n${err.stack}`))
+
