88b0d964eecce6ed31af737892c03cad40809855
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-01-17 18:26:33 -0500

    fix(testContext): allow async fn to get context

--- app_parser/src/_helpers/testContext.ts
+++ app_parser/src/_helpers/testContext.ts
@@ -1,8 +1,8 @@
 import * as ava from 'ava'
 
-export function contextualize<T>(getContext: () => T): ava.RegisterContextual<T> {
-  ava.test.beforeEach(t => {
-    Object.assign(t.context, getContext())
+export function contextualize<T>(getContext: () => T | Promise<T>): ava.RegisterContextual<T> {
+  ava.test.beforeEach(async t => {
+    Object.assign(t.context, await getContext())
   })
 
   return ava.test
