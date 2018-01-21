0183fa55a9550d5bb9d40fccbd9fab97458aad74
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-01-20 23:18:26 -0500

    feat(_helpers): add fn returning object with spied property
    The property is spied on get and on set. So when some code is trying to get
    value or set value, spy will be triggered.

---
+++ app_parser/src/_helpers/objectWithPropertySpy.ts
@@ -0,0 +1,32 @@
+import { SinonStub, stub } from 'sinon'
+
+
+export const objectWithPropertySpy = <T>(value: T): {
+  getSpy: SinonStub,
+  setSpy: SinonStub,
+  propName: 'prop',
+  obj: { prop: T },
+} => {
+
+  let valStorage = value
+
+  const getSpy = stub().callsFake(() => {
+    return valStorage
+  })
+  const setSpy = stub().callsFake((newValue: T) => {
+    valStorage = newValue
+  })
+  const obj = Object.defineProperty({}, 'prop', {
+    configurable: true,
+    enumerable: true,
+    set: setSpy,
+    get: getSpy,
+  })
+
+  return {
+    getSpy,
+    setSpy,
+    propName: 'prop',
+    obj,
+  }
+}
