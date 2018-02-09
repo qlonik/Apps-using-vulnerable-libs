2043b6ca8e7e8b1ed29ba20fe0282f3fc03d0ff2
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-02-09 00:36:36 -0500

    feat(dumpWatch:common): add new server/client message types
    Two message types are introduced - serverMessage2 and clientMessage2. These new
    messages will replace old message types (serverMessage1 and clientMessage1).
    New message types contain direction of the message, id of the message, and data
    of the message. In these two new types, data field corresponds to object of old
    message type (minus the direction of the message). This change also adds
    functions intended to distinguish between two types of messages.

--- app_parser/src/dumpWatch/common.ts
+++ app_parser/src/dumpWatch/common.ts
@@ -18,12 +18,34 @@ export enum serverMessageType {
   shutdown,
 }
 
-export type serverMessage = { from: messageFrom.server } & (
+export type serverMessage = serverMessage1 | serverMessage2
+
+export type serverMessage1 = { from: messageFrom.server } & (
   { type: serverMessageType.startup } |
   ({ type: serverMessageType.process } & processRequest) |
   ({ type: serverMessageType.reanalyseLib } & reanalyseLibRequest) |
   { type: serverMessageType.shutdown }
   )
+
+export type serverMessage2Data = (
+  { type: serverMessageType.startup } |
+  ({ type: serverMessageType.process } & processRequest) |
+  ({ type: serverMessageType.reanalyseLib } & reanalyseLibRequest) |
+  { type: serverMessageType.shutdown }
+  )
+export type serverMessage2 = {
+  from: messageFrom.server,
+  id: string,
+  data: serverMessage2Data
+}
+
+export function isServerMessage1(m: serverMessage): m is serverMessage1 {
+  return ('type' in m)
+}
+
+export function isServerMessage2(m: serverMessage): m is serverMessage2 {
+  return ('id' in m)
+}
 
 export type processRequest = {
   libsPath: string,
@@ -44,12 +66,34 @@ export enum clientMessageType {
   delayShutdown,
 }
 
-export type clientMessage = { from: messageFrom.client } & (
+export type clientMessage = clientMessage1 | clientMessage2
+
+export type clientMessage1 = { from: messageFrom.client } & (
   { type: clientMessageType.startupDone } |
   ({ type: clientMessageType.processingResult } & processingResult) |
   ({ type: clientMessageType.reanalysisResult } & reanalysisResult) |
   { type: clientMessageType.delayShutdown }
   )
+
+export type clientMessage2Data = (
+  { type: clientMessageType.startupDone } |
+  ({ type: clientMessageType.processingResult } & processingResult) |
+  ({ type: clientMessageType.reanalysisResult } & reanalysisResult) |
+  { type: clientMessageType.delayShutdown }
+  )
+export type clientMessage2 = {
+  from: messageFrom.client,
+  id: string,
+  data: clientMessage2Data
+}
+
+export function isClientMessage1(m: clientMessage): m is clientMessage1 {
+  return ('type' in m)
+}
+
+export function isClientMessage2(m: clientMessage): m is clientMessage2 {
+  return ('id' in m)
+}
 
 export type processingResult = {
   filename: string,
