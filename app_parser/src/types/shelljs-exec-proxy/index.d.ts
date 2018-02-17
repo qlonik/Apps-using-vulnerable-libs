0ddc35d4dab914e5a40b8c95179b10753a0fc9dd
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-02-17 15:51:27 -0500

    fix(types/shelljs-exec-proxy): exec only accepts strings
    Before function was incorrectly typed.

--- app_parser/src/types/shelljs-exec-proxy/index.d.ts
+++ app_parser/src/types/shelljs-exec-proxy/index.d.ts
@@ -1,19 +1,8 @@
 declare module 'shelljs-exec-proxy' {
-  import { ChildProcess } from 'child_process'
   import shelljs = require('shelljs');
 
   interface exec {
-    (command?: string): shelljs.ExecOutputReturnValue;
-
-    (options: shelljs.ExecOptions): shelljs.ExecOutputReturnValue | ChildProcess;
-    (command: string, options: shelljs.ExecOptions): shelljs.ExecOutputReturnValue | ChildProcess;
-
-    (options: shelljs.ExecOptions, callback: shelljs.ExecCallback): ChildProcess;
-    (command: string, options: shelljs.ExecOptions, callback: shelljs.ExecCallback): ChildProcess;
-
-    (callback: shelljs.ExecCallback): ChildProcess;
-    (command: string, callback: shelljs.ExecCallback): ChildProcess;
-
+    (...command: string[]): shelljs.ExecOutputReturnValue;
     [k: string]: exec;
   }
 
