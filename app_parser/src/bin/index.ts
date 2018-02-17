275e2c8ea0145d241a178108f35c89847a22e444
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-02-16 20:24:02 -0500

    feat(bin): create ./bin script runner
    This allows us to have one entry file, which will call
    other file, which was requested.

---
+++ app_parser/src/bin/index.ts
@@ -0,0 +1,29 @@
+import { resolve } from 'path'
+import Module = require('module')
+import yargs = require('yargs')
+
+
+const argv = yargs
+  .command('$0 <script>', 'execute bin script', (yargs) => {
+    return yargs.positional('script', {
+      desc: 'filename of the script',
+      type: 'string',
+    })
+  })
+  .help()
+  .argv
+
+
+const replExt = argv.script.replace(/.(t|j)sx?$/, '.js')
+const jsScript = `${replExt}${replExt.endsWith('.js') ? '' : '.js'}`
+const scriptPath = resolve(__dirname, jsScript)
+
+process.argv = [
+  process.argv[0],
+  scriptPath,
+]
+// Magic found in npx source code:
+// https://github.com/zkat/npx/blob/357e6abc49077d7e4325406852d182220816e4f2/index.js#L264
+Module.runMain()
+
+
