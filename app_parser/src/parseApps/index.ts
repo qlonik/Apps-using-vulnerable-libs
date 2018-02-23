2f6d3ab0ac903e4e2963d6b6e1ec450d9961e370
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-02-22 20:25:43 -0500

    chore(parseApps): remove old unused fn and type

--- app_parser/src/parseApps/index.ts
+++ app_parser/src/parseApps/index.ts
@@ -1,12 +1,5 @@
-import { readdir } from 'fs-extra'
-import { join } from 'path'
 import { opts } from '../utils'
 
-
-export interface AppDescription {
-  section: string,
-  app: string,
-}
 
 export type IsAppTypeFn =
   (p: { appPath: string }) => Promise<boolean>
@@ -16,18 +9,6 @@ export type AppsFolderParserFn =
 
 export type AppParserFn =
   (p: { appPath: string, libsPath: string }, opts?: opts) => Promise<any>
-
-
-export async function getAppsAndSections(
-  { allAppsPath }: { allAppsPath: string }): Promise<AppDescription[]> {
-
-  const appSections = await readdir(allAppsPath)
-  const appsNonFlat = await Promise.all(appSections.map(async (appSection) => {
-    const apps = await readdir(join(allAppsPath, appSection))
-    return apps.map((app) => ({ section: appSection, app }))
-  }))
-  return (<AppDescription[]>[]).concat(...appsNonFlat)
-}
 
 
 export * from './getters'
