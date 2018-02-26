a17b61af651ba2018847fd02566af89f7ce3cc9b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-02-25 21:02:13 -0500

    feat(parseApps:cordova): preprocessCordovaApp()

--- app_parser/src/parseApps/cordova.ts
+++ app_parser/src/parseApps/cordova.ts
@@ -1,5 +1,6 @@
 import { mkdirp, pathExists, readFile } from 'fs-extra'
 import { JSDOM } from 'jsdom'
+import { flatten } from 'lodash'
 import { join } from 'path'
 import { URL } from 'url'
 import { extractStructure } from '../extractStructure'
@@ -7,8 +8,8 @@ import { getSimilarityToLibs } from '../similarityIndex'
 import { leftPad, opts, resolveAllOrInParallel } from '../utils'
 import { fileDescOp, fileOp, saveFiles } from '../utils/files'
 import { stdoutLog } from '../utils/logger'
-import { APP_TYPES, getApps } from './getters'
-import { AppParserFn, AppsFolderParserFn, IsAppTypeFn } from '.'
+import { APP_TYPES, appDesc, getApps } from './getters'
+import { AppParserFn, AppsFolderParserFn, IsAppTypeFn } from './index'
 
 const NAMESPACE = 'cordova'
 const log = stdoutLog(NAMESPACE)
@@ -136,6 +137,123 @@ export const parseScriptsFromCordovaApp: AppParserFn = async (
     contents = await resolveAllOrInParallel(lazyScriptTags, { chunkLimit, chunkSize })
   }
   // console.log(contents.map(s => s.length <= 1000 ? s : s.length))
+}
+
+export const preprocessCordovaApp = async (
+  {
+    allAppsPath,
+    app: { type, section, app },
+  }: {
+    allAppsPath: string
+    app: appDesc
+  },
+  { conservative = true }: opts = {},
+) => {
+  const appPath = join(allAppsPath, type, section, app)
+  const indexHtmlPath = join(appPath, 'js', 'index.html')
+  const jsAnalysisPath = join(appPath, 'an')
+  await mkdirp(jsAnalysisPath)
+  const { window: { document } } = await JSDOM.fromFile(indexHtmlPath)
+  const parseScriptTags = flatten(
+    ['head', 'body'].map((location) => {
+      const scriptTags = document.querySelectorAll(`${location} script`) as NodeListOf<
+        HTMLScriptElement
+      >
+
+      return [...scriptTags].map((script: HTMLScriptElement, i) => {
+        return async () => {
+          const cwd = join(jsAnalysisPath, location, leftPad(i))
+          const fileOps: fileDescOp[] = []
+
+          /**
+           * Important object
+           */
+          let infoObject: {
+            scriptTagLocation: string
+            scriptTagIndex: number
+          } & (
+            | {
+                type: 'src'
+                originalSrc: string
+                originalPath: string
+                originalProtocol: string
+              }
+            | {
+                type: 'content'
+              }
+            | {
+                type: 'unknown'
+                tagKeys: string[]
+              })
+          let content: string = ''
+
+          if (script.src) {
+            const url = new URL(script.src)
+            infoObject = {
+              scriptTagLocation: location,
+              scriptTagIndex: i,
+
+              type: 'src',
+              originalSrc: script.src,
+              originalPath: url.pathname,
+              originalProtocol: url.protocol,
+            }
+
+            if (url.protocol === 'file:') {
+              fileOps.push({
+                cwd,
+                dst: 'libDesc.js',
+                type: fileOp.copy,
+                src: url.pathname,
+                conservative,
+              })
+              content = await readFile(url.pathname, 'utf-8')
+            } else if (url.protocol === 'http:' || url.protocol === 'https:') {
+              log('script referenced via http / https!')
+            } else {
+              log(`%o script #%o has an unknown src!`, location, i)
+            }
+          } else if (script.text) {
+            infoObject = {
+              scriptTagLocation: location,
+              scriptTagIndex: i,
+
+              type: 'content',
+            }
+
+            fileOps.push({
+              cwd,
+              dst: 'libDesc.js',
+              type: fileOp.text,
+              text: script.text,
+              conservative,
+            })
+            content = script.text
+          } else {
+            log(`something unknown with %o script #%o`, location, i)
+            infoObject = {
+              scriptTagLocation: location,
+              scriptTagIndex: i,
+
+              type: 'unknown',
+              tagKeys: Object.keys(script),
+            }
+          }
+
+          const signature = await extractStructure({ content })
+
+          return await saveFiles(
+            fileOps.concat([
+              { cwd, dst: 'info.json', type: fileOp.json, json: infoObject, conservative },
+              { cwd, dst: 'libStructure.json', type: fileOp.json, json: signature, conservative },
+            ]),
+          )
+        }
+      })
+    }),
+  )
+
+  await resolveAllOrInParallel(parseScriptTags)
 }
 
 export const parseScriptsFromCordovaApps: AppsFolderParserFn = async (
