f7d62fbabded788e55b902ae8c7b143058d574a2
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-01 15:21:17 -0500

    feat(bin/data-status): list all apps in data pool

--- app_parser/src/bin/data-status.ts
+++ app_parser/src/bin/data-status.ts
@@ -1,9 +1,12 @@
+import { pathExists, readdir } from 'fs-extra'
+import { join } from 'path'
 import { APP_TYPES, getApps } from '../parseApps'
 import { getLibNameVersions } from '../parseLibraries'
 import { stdoutLog } from '../utils/logger'
 
 const LIB_FOLDER = '../data/sample_libs'
 const APP_FOLDER = '../data/sample_apps'
+const APP_POOL_FOLDER = '/gi-pool/appdata-ro'
 
 const log = stdoutLog('data-status')
 log.enabled = true
@@ -16,6 +19,19 @@ async function main() {
   log('total libs+vers:     %o', libs.length)
   log('total cordova:       %o', cordovaApps.length)
   log('total react-native:  %o', rnApps.length)
+
+  if (!await pathExists(APP_FOLDER)) {
+    return
+  }
+
+  const sections = await readdir(APP_POOL_FOLDER)
+  const apps = await sections.reduce(async (acc, section) => {
+    const prev = await acc
+    const sectionApps = await readdir(join(APP_POOL_FOLDER, section))
+    return prev.concat(sectionApps.map((app) => ({ section, app })))
+  }, Promise.resolve([] as { section: string; app: string }[]))
+
+  log('total apps in pool:  %o', apps.length)
 }
 
 main().catch((err) => log('Some global error:\n%s', err.stack))
