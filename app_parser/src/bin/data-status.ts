d6b0eef7f96839529527a1d1792b773be7f7a416
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-01 15:24:13 -0500

    refactor(bin/data-status): report only if folders exist

--- app_parser/src/bin/data-status.ts
+++ app_parser/src/bin/data-status.ts
@@ -12,26 +12,30 @@ const log = stdoutLog('data-status')
 log.enabled = true
 
 async function main() {
-  const libs = await getLibNameVersions(LIB_FOLDER)
-  const cordovaApps = await getApps(APP_FOLDER, APP_TYPES.cordova)
-  const rnApps = await getApps(APP_FOLDER, APP_TYPES.reactNative)
+  if (await pathExists(LIB_FOLDER)) {
+    const libs = await getLibNameVersions(LIB_FOLDER)
 
-  log('total libs+vers:     %o', libs.length)
-  log('total cordova:       %o', cordovaApps.length)
-  log('total react-native:  %o', rnApps.length)
-
-  if (!await pathExists(APP_FOLDER)) {
-    return
+    log('total libs+vers:     %o', libs.length)
   }
 
-  const sections = await readdir(APP_POOL_FOLDER)
-  const apps = await sections.reduce(async (acc, section) => {
-    const prev = await acc
-    const sectionApps = await readdir(join(APP_POOL_FOLDER, section))
-    return prev.concat(sectionApps.map((app) => ({ section, app })))
-  }, Promise.resolve([] as { section: string; app: string }[]))
+  if (await pathExists(APP_FOLDER)) {
+    const cordovaApps = await getApps(APP_FOLDER, APP_TYPES.cordova)
+    const rnApps = await getApps(APP_FOLDER, APP_TYPES.reactNative)
 
-  log('total apps in pool:  %o', apps.length)
+    log('total cordova:       %o', cordovaApps.length)
+    log('total react-native:  %o', rnApps.length)
+  }
+
+  if (await pathExists(APP_FOLDER)) {
+    const sections = await readdir(APP_POOL_FOLDER)
+    const apps = await sections.reduce(async (acc, section) => {
+      const prev = await acc
+      const sectionApps = await readdir(join(APP_POOL_FOLDER, section))
+      return prev.concat(sectionApps.map((app) => ({ section, app })))
+    }, Promise.resolve([] as { section: string; app: string }[]))
+
+    log('total apps in pool:  %o', apps.length)
+  }
 }
 
 main().catch((err) => log('Some global error:\n%s', err.stack))
