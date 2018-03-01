86d7abdcc1fdc4476051e6dbb4a44cad9efea590
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-01 15:12:30 -0500

    refactor(bin/data-status): use debug logger for status info

--- app_parser/src/bin/data-status.ts
+++ app_parser/src/bin/data-status.ts
@@ -1,17 +1,21 @@
 import { APP_TYPES, getApps } from '../parseApps'
 import { getLibNameVersions } from '../parseLibraries'
+import { stdoutLog } from '../utils/logger'
 
 const LIB_FOLDER = '../data/sample_libs'
 const APP_FOLDER = '../data/sample_apps'
+
+const log = stdoutLog('data-status')
+log.enabled = true
 
 async function main() {
   const libs = await getLibNameVersions(LIB_FOLDER)
   const cordovaApps = await getApps(APP_FOLDER, APP_TYPES.cordova)
   const rnApps = await getApps(APP_FOLDER, APP_TYPES.reactNative)
 
-  console.log(`total libs+vers:  ${libs.length}`)
-  console.log(`total apps (cd):  ${cordovaApps.length}`)
-  console.log(`total apps (rn):  ${rnApps.length}`)
+  log('total libs+vers:  %o', libs.length)
+  log('total apps (cd):  %o', cordovaApps.length)
+  log('total apps (rn):  %o', rnApps.length)
 }
 
-main().catch((err) => console.log('Some global error:\n' + err.stack))
+main().catch((err) => log('Some global error:\n%s', err.stack))
