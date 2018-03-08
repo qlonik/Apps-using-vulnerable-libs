4c01377d5c1b7640180ec3062d4ec0cd15156512
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-08 18:46:07 -0500

    feat(util/errors): add error types

---
+++ app_parser/src/utils/errors.ts
@@ -0,0 +1,34 @@
+export class DataError extends Error {
+  public constructor(msg?: string) {
+    super(msg)
+    this.name = 'DataError'
+  }
+}
+
+export class AppDataError extends DataError {
+  public constructor(msg?: string) {
+    super(msg)
+    this.name = 'AppDataError'
+  }
+}
+
+export class CordovaAppDataError extends AppDataError {
+  public constructor(msg?: string) {
+    super(msg)
+    this.name = 'CordovaAppDataError'
+  }
+}
+
+export class ReactNativeAppDataError extends AppDataError {
+  public constructor(msg?: string) {
+    super(msg)
+    this.name = 'ReactNativeAppDataError'
+  }
+}
+
+export class LibDataError extends DataError {
+  public constructor(msg?: string) {
+    super(msg)
+    this.name = 'LibDataError'
+  }
+}
