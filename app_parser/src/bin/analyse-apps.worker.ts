8d553ded31086b68bb6e27439e14217548602fbc
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-09 14:56:17 -0500

    feat(bin/analyse-apps.worker): analyse cordova apps

--- app_parser/src/bin/analyse-apps.worker.ts
+++ app_parser/src/bin/analyse-apps.worker.ts
@@ -1,8 +1,30 @@
 import { worker } from 'workerpool'
+import { analyseCordovaApp, APP_TYPES } from '../parseApps'
+import { stdoutLog } from '../utils/logger'
 import { messages } from './analyse-apps'
+
+const log = stdoutLog(`analyse-apps:worker:${process.pid}`)
 
 worker<messages>({
   analyse: async ({ allAppsPath, allLibsPath, app }) => {
+    if (app.type === APP_TYPES.reactNative) {
+      return false
+    }
+
+    if (app.type === APP_TYPES.cordova) {
+      try {
+        log('started  analysing %o', `${app.type}/${app.section}/${app.app}`)
+        await analyseCordovaApp({ allAppsPath, libsPath: allLibsPath, app })
+        log('finished analysing %o', `${app.type}/${app.section}/${app.app}`)
+        return true
+      } catch (err) {
+        log(err)
+        return false
+      }
+    }
+
     return false
   },
 })
+
+process.on('SIGINT', () => {})
