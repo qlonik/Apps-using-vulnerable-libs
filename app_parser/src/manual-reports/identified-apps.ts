26978cd64025206852d6503315e5f2cc3b9f461e
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-09 15:42:54 -0500

    feat(manual-reports): create initial type for ManuallyAnalysedApps

---
+++ app_parser/src/manual-reports/identified-apps.ts
@@ -0,0 +1,20 @@
+import { appDesc } from '../parseApps'
+import { libNameVersionSigFile } from '../parseLibraries'
+
+export interface ManuallyAnalysedApps {
+  [name: string]: {
+    app: appDesc
+    files: {
+      [id: string]: {
+        location: string
+        id: string
+      } & (
+        | { type: 'unknown' | ''; match?: any }
+        | { type: 'business-logic' }
+        | { type: 'single-lib'; match: libNameVersionSigFile }
+        | { type: 'bundle'; match: libNameVersionSigFile[] })
+    }
+  }
+}
+
+export const appsAnalysed: ManuallyAnalysedApps = {}
