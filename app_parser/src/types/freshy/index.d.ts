e0d01283fc888a3c6e655b9375b4df399b1c57b5
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-12 01:49:03 -0400

    feat(package.json): add module to unload other modules

---
+++ app_parser/src/types/freshy/index.d.ts
@@ -0,0 +1,5 @@
+declare module 'freshy' {
+  export function unload(module: string): boolean
+  export function reload(module: string): any
+  export function freshy(module: string, cb?: (module: any) => void): any
+}
