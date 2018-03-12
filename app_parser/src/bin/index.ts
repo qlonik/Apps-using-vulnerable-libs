95581a8c969ae178dc5c8d4272f77254980190f5
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-12 00:32:21 -0400

    refactor(bin/index): move command handler into yargs handler fn
    Yargs allows providing handler function for command, which will be called when
    the given command is executed.

--- app_parser/src/bin/index.ts
+++ app_parser/src/bin/index.ts
@@ -3,21 +3,26 @@ import Module from 'module'
 import { resolve } from 'path'
 import * as yargs from 'yargs'
 
-const argv = yargs
-  .command('$0 <script>', 'execute bin script', (yargs) => {
-    return yargs.positional('script', {
-      desc: 'filename of the script',
-      type: 'string',
-    })
-  })
+yargs
+  .command(
+    '$0 <script>',
+    'execute bin script',
+    (yargs) => {
+      return yargs.positional('script', {
+        desc: 'filename of the script',
+        type: 'string',
+      })
+    },
+    async (args) => {
+      const kebabCaseName = kebabCase(args.script)
+      const replExt = kebabCaseName.replace(/.(t|j)sx?$/, '.js')
+      const jsScript = `${replExt}${replExt.endsWith('.js') ? '' : '.js'}`
+      const scriptPath = resolve(__dirname, jsScript)
+
+      process.argv = [process.argv[0], scriptPath]
+      // Magic found in npx source code:
+      // https://github.com/zkat/npx/blob/357e6abc49077d7e4325406852d182220816e4f2/index.js#L264
+      Module.runMain()
+    },
+  )
   .help().argv
-
-const kebabCaseName = kebabCase(argv.script)
-const replExt = kebabCaseName.replace(/.(t|j)sx?$/, '.js')
-const jsScript = `${replExt}${replExt.endsWith('.js') ? '' : '.js'}`
-const scriptPath = resolve(__dirname, jsScript)
-
-process.argv = [process.argv[0], scriptPath]
-// Magic found in npx source code:
-// https://github.com/zkat/npx/blob/357e6abc49077d7e4325406852d182220816e4f2/index.js#L264
-Module.runMain()
