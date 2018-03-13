261fd1f61d56380f1354c4a5b9b8c320f43918ec
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-13 19:06:27 -0400

    style(manual-reports): format ManuallyAnalysedApps uniformly

--- app_parser/src/manual-reports/identified-apps.ts
+++ app_parser/src/manual-reports/identified-apps.ts
@@ -5,10 +5,11 @@ export interface ManuallyAnalysedApps {
   [name: string]: {
     app: appDesc
     files: {
-      [id: string]: (
-        | { location: string; id: string }
-        | { id: string; idType: 's' }
-        | { id: number; idType: 'n' }) &
+      [id: string]: /* empty obj so prettier formats uniformly */ ({}) &
+        (
+          | { location: string; id: string }
+          | { id: string; idType: 's' }
+          | { id: number; idType: 'n' }) &
         (
           | { type: 'unknown' | ''; match?: any }
           | { type: 'business-logic' }
