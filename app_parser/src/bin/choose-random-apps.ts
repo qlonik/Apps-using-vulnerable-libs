0b58bbb76f636305721d4302f6febd90b45573e8
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-03-12 23:59:21 -0400

    refactor(bin/random-apps): save chosen apps as map

--- app_parser/src/bin/choose-random-apps.ts
+++ app_parser/src/bin/choose-random-apps.ts
@@ -1,14 +1,32 @@
 import { readJSON } from 'fs-extra'
 import { random } from 'lodash'
 import { join } from 'path'
+import { appDesc } from '../parseApps'
 import { LIB_CANDIDATES_FILE } from '../parseApps/constants'
 import { myWriteJSON } from '../utils/files'
-import { AppCandidateMap } from './compute-candidates'
+import { AppCandidateMap, CandidateMap } from './compute-candidates'
 
 const ALL_APPS_PATH = '../data/sample_apps'
 const LIB_CANDIDATES_PATH = join(ALL_APPS_PATH, LIB_CANDIDATES_FILE)
 const RANDOM_100_PATH = join(ALL_APPS_PATH, '../random_100.json')
 const RANDOM_10_PATH = join(ALL_APPS_PATH, '../random_10.json')
+
+interface AppFilesMap {
+  [id: string]: {
+    app: appDesc
+    files: CandidateMap
+  }
+}
+
+function transformToMap(ids: string[], origData: AppCandidateMap) {
+  return ids.sort().reduce(
+    (acc, id) => {
+      const { app, candidates } = origData[id]
+      return { ...acc, [id]: { app, files: candidates } }
+    },
+    {} as AppFilesMap,
+  )
+}
 
 export async function main() {
   const appCandidates = (await readJSON(LIB_CANDIDATES_PATH)) as AppCandidateMap
@@ -25,8 +43,8 @@ export async function main() {
     res10 = res10.concat(res100copy.splice(rndI, 1))
   }
 
-  const cand100 = res100.map((id) => appCandidates[id])
-  const cand10 = res10.map((id) => appCandidates[id])
+  const cand100 = transformToMap(res100, appCandidates)
+  const cand10 = transformToMap(res10, appCandidates)
 
   await myWriteJSON({ content: cand100, file: RANDOM_100_PATH })
   await myWriteJSON({ content: cand10, file: RANDOM_10_PATH })
