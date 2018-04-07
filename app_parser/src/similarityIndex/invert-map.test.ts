0fb6fb39acda7a0b88ee38685427dfc6c662dd51
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-04-07 00:55:16 -0400

    feat(invert-map.test): add generative map inversion check

--- app_parser/src/similarityIndex/invert-map.test.ts
+++ app_parser/src/similarityIndex/invert-map.test.ts
@@ -1,4 +1,7 @@
 import { test } from 'ava'
+import { unzip, zip } from 'lodash'
+import { identity, uniqBy } from 'lodash/fp'
+import { arb, check } from '../_helpers/property-test'
 import { invertMap } from './set'
 
 test('map is inverted', t => {
@@ -6,6 +9,20 @@ test('map is inverted', t => {
   const inverted = new Map([[2, 1], [3, 2], [1, 3]])
   t.deepEqual(inverted, invertMap(map))
 })
+
+const setOfIntegerPairs = arb
+  .nearray(arb.pair(arb.nat, arb.nat))
+  .smap(uniqBy<[number, number]>(x => x[0]), identity)
+  .smap(uniqBy<[number, number]>(x => x[1]), identity)
+
+test(
+  'map gets inverted',
+  check(setOfIntegerPairs, (t, pairs: [number, number][]) => {
+    const unzipped = unzip(pairs)
+    const inverted = zip(unzipped[1], unzipped[0]) as [number, number][]
+    t.deepEqual(new Map(pairs), invertMap(new Map(inverted)))
+  }),
+)
 
 test('throws when values are not unique', t => {
   const map = new Map([[1, 2], [2, 2], [3, 2]])
