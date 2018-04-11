fb0562df4ceeece36c6899dea6a71fd48b1f32ac
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-04-10 21:49:54 -0400

    feat(similarityIndex.test): add Arbitrary Comment and FunctionSignature

--- app_parser/src/similarityIndex/similarity-methods/_test-data.ts
+++ app_parser/src/similarityIndex/similarity-methods/_test-data.ts
@@ -1,11 +1,33 @@
 import arb from 'jsverify'
-import { FunctionSignature, LiteralSignature } from '../../extractStructure'
+import {
+  CommentSignature,
+  fnNamesConcat,
+  fnNamesSplit,
+  FunctionSignature,
+  LiteralSignature,
+} from '../../extractStructure'
 import { jaccardLike } from '../set'
+
+export const arbFunctionSignature = arb.record({
+  type: arb.constant('fn'),
+  name: arb
+    .nearray(arb.oneof([arb.asciinestring, arb.constant('[anonymous]')]))
+    .smap((a) => a.reduce(fnNamesConcat, ''), fnNamesSplit),
+  fnStatementTokens: arb.array(arb.asciinestring),
+  fnStatementTypes: arb.array(arb.asciinestring),
+}) as arb.Arbitrary<FunctionSignature>
+export const arbFunctionSignatureArr = arb.nearray(arbFunctionSignature)
 
 export const arbLiteralSignature = arb.oneof<any>([arb.number, arb.asciistring]) as arb.Arbitrary<
   LiteralSignature
 >
 export const arbLiteralSignatureArr = arb.nearray(arbLiteralSignature)
+
+export const arbCommentSignature = arb.oneof<any>([
+  arb.asciinestring,
+  arb.nearray(arb.asciinestring),
+]) as arb.Arbitrary<CommentSignature>
+export const arbCommentSignatureArr = arb.nearray(arbCommentSignature)
 
 export const UNKNOWN_SIG: FunctionSignature[] = [
   {
