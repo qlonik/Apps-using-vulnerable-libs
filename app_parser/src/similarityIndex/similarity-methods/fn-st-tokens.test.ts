18e28a93f5817da380f47fe176c4926e81def74f
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-04-13 00:39:46 -0400

    feat(fnStToks.test): check that v3 is commutative

--- app_parser/src/similarityIndex/similarity-methods/fn-st-tokens.test.ts
+++ app_parser/src/similarityIndex/similarity-methods/fn-st-tokens.test.ts
@@ -1,5 +1,8 @@
 import { test } from 'ava'
 import { cloneDeep } from 'lodash'
+import { arbFunctionSignatureArr } from '../../_helpers/arbitraries'
+import { check } from '../../_helpers/property-test'
+import { invertMapWithConfidence } from '../set'
 import { EXPECTED_MAPPING, EXPECTED_SIMILARITY, LIB_SIG, UNKNOWN_SIG } from './_test-data'
 import { v1, v2, v3 } from './fn-st-tokens'
 
@@ -20,3 +23,13 @@ test('v3', t => {
   t.deepEqual(EXPECTED_SIMILARITY, similarity)
   t.deepEqual(EXPECTED_MAPPING, mapping)
 })
+
+test(
+  'v3 is commutative',
+  check({ size: 200 }, arbFunctionSignatureArr, arbFunctionSignatureArr, (t, x, y) => {
+    const { similarity: sim_xy, mapping: map_xy } = v3(x, y)
+    const { similarity: sim_yx, mapping: map_yx } = v3(y, x)
+    t.deepEqual(sim_xy, sim_yx)
+    t.deepEqual(map_xy, invertMapWithConfidence(map_yx))
+  }),
+)
