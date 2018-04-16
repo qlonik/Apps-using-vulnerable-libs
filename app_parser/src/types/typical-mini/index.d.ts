bc9f58ba16f09b0a8231bd9f6c6765f6d886e01b
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-04-15 21:37:51 -0400

    feat(types/typical-mini): add Omit and Simplify types

--- app_parser/src/types/typical-mini/index.d.ts
+++ app_parser/src/types/typical-mini/index.d.ts
@@ -33,4 +33,7 @@ declare module 'typical-mini' {
     // prettier-ignore
     10: (a0: A[0], a1: A[1], a2: A[2], a3: A[3], a4: A[4], a5: A[5], a6: A[6], a7: A[7], a8: A[8], a9: A[9]) => R
   }[The<number, Length<A>>]
+
+  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+  type Simplify<T> = Pick<T, keyof T>
 }
