3d66f7c3e9c4433e08132da881534245ebf7f9f5
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-04-28 02:15:50 -0400

    test(fnStToks): check nested block is parsed

--- app_parser/src/extractStructure/fn-statement-tokens.test.ts
+++ app_parser/src/extractStructure/fn-statement-tokens.test.ts
@@ -439,6 +439,29 @@ test(
 )
 
 test(
+  'if: if else {if {expr} else}',
+  checkTokensMacro,
+  stripIndent`
+    function a() {
+      if (i === 1) {}
+      else {
+        if (i === 2) {
+          b = 123
+        }
+        else {}
+      }
+    }
+  `,
+  [
+    `${STATEMENT}:If`,
+    `${STATEMENT}:Else`,
+    `${STATEMENT}:If`,
+    `${EXPRESSION}:Assignment[${PARAM}:Identifier[b] = ${LITERAL}:Numeric]`,
+    `${STATEMENT}:Else`,
+  ],
+)
+
+test(
   'if: if else-if else-if else',
   checkTokensMacro,
   stripIndent`
