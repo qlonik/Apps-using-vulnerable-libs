51fd211ff3ebfac4eef7bc457eedc11cbd240ee9
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-05-03 00:17:36 -0400

    feat(bin): remove support for old run script mode
    In new mode, the script has to export async main() fn

--- app_parser/src/types/freshy/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'freshy' {
-  export function unload(module: string): boolean
-  export function reload(module: string): any
-  export function freshy(module: string, cb?: (module: any) => void): any
-}
