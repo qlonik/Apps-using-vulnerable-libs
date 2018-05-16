25531e1169eaeb39d39e84fd244bba833c8530b5
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-05-16 19:15:58 -0400

    feat(utils/worker): helper workerpool() with proper stdio

--- app_parser/src/utils/worker.ts
+++ app_parser/src/utils/worker.ts
@@ -1,5 +1,7 @@
 import { pathExists } from 'fs-extra'
 import { basename, dirname, extname, join } from 'path'
+import { pool, PoolOptions } from 'workerpool'
+import { fd } from './logger'
 
 export const getWorkerPath = async (file: string): Promise<string> => {
   const ext = extname(file)
@@ -11,4 +13,27 @@ export const getWorkerPath = async (file: string): Promise<string> => {
   } else {
     throw new Error('no worker present')
   }
+}
+
+export const poolFactory: typeof pool = (str?: string | PoolOptions, opts?: PoolOptions) => {
+  let script: string | null
+  if (typeof str === 'string') {
+    script = str || null
+  } else {
+    script = null
+    opts = str
+  }
+  opts = opts || {}
+
+  const options: PoolOptions = {
+    ...opts,
+    forkOpts: {
+      ...('forkOpts' in opts ? opts.forkOpts : {}),
+      stdio: ['inherit', 'inherit', 'inherit', 'ipc'].concat(
+        fd ? new Array(fd - 4).fill(null).concat(fd) : [],
+      ),
+    },
+  }
+
+  return script === null ? pool(options) : pool(script, options)
 }
