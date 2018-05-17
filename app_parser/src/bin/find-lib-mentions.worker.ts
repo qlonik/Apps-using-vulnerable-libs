a1d532c22c9c90b79c3ccbff6438a31ce97198aa
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-05-17 12:56:03 -0400

    refactor(find-lib-mentions.worker): change concurrent search in sequential
    After refactor, the for-of loop is in use, and only one iteration at a time is
    processed, i.e. concurrency of resolveAllOrInParallel is gone.

--- app_parser/src/bin/find-lib-mentions.worker.ts
+++ app_parser/src/bin/find-lib-mentions.worker.ts
@@ -3,7 +3,6 @@ import { readJSON } from 'fs-extra'
 import { includes } from 'lodash'
 import { worker } from 'workerpool'
 import { APP_TYPES, getAnalysedData, getCordovaAnalysisFiles } from '../parseApps'
-import { resolveAllOrInParallel } from '../utils'
 import { foundMentionsMap, messages, npmLibs, regexLibs } from './find-lib-mentions'
 
 const NAME_VERSION_REGEX = /([\w-]+)\s+(?:@?version\s+)?(v?\d+\.\d+\.\d+)/g
@@ -28,53 +27,48 @@ worker<messages>({
         await getCordovaAnalysisFiles(APPS_PATH, app),
       )
 
-      const found = await resolveAllOrInParallel(
-        analysedFiles.map(({ file, signature }) => async () => {
-          if (signature === null) {
-            return false
+      const found = {} as foundMentionsMap
+      for (let { file, signature } of analysedFiles) {
+        if (signature === null) {
+          continue
+        }
+
+        const regexLibsMap = new Map<regexLibs[0], regexLibs[1]>()
+        const npmLibsMap = new Map<npmLibs[0], npmLibs[1]>()
+
+        for (let comment of signature.comments) {
+          const commentStr = Array.isArray(comment) ? comment.join('\n') : comment
+
+          let foundMatch
+          // This while loop finds all matches of regex in the string. Found on MDN:
+          // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec#Examples
+          while ((foundMatch = NAME_VERSION_REGEX.exec(commentStr)) !== null) {
+            const [, name, version] = foundMatch
+            const { count, versions } = regexLibsMap.get(name) || { count: 0, versions: [] }
+            regexLibsMap.set(name, {
+              count: count + 1,
+              versions: includes(versions, version) ? versions : versions.concat(version),
+            })
           }
+          NAME_VERSION_REGEX.lastIndex = 0
 
-          const regexLibsMap = new Map<regexLibs[0], regexLibs[1]>()
-          const npmLibsMap = new Map<npmLibs[0], npmLibs[1]>()
-
-          for (let comment of signature.comments) {
-            const commentStr = Array.isArray(comment) ? comment.join('\n') : comment
-
-            let foundMatch
-            // This while loop finds all matches of regex in the string. Found on MDN:
-            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec#Examples
-            while ((foundMatch = NAME_VERSION_REGEX.exec(commentStr)) !== null) {
-              const [, name, version] = foundMatch
-              const { count, versions } = regexLibsMap.get(name) || { count: 0, versions: [] }
-              regexLibsMap.set(name, {
-                count: count + 1,
-                versions: includes(versions, version) ? versions : versions.concat(version),
-              })
+          for (let { name, reg } of namesRegArr) {
+            // same while loop as above
+            while (reg.exec(commentStr) !== null) {
+              const { count } = npmLibsMap.get(name) || { count: 0 }
+              npmLibsMap.set(name, { count: count + 1 })
             }
-            NAME_VERSION_REGEX.lastIndex = 0
-
-            for (let { name, reg } of namesRegArr) {
-              // same while loop as above
-              while (reg.exec(commentStr) !== null) {
-                const { count } = npmLibsMap.get(name) || { count: 0 }
-                npmLibsMap.set(name, { count: count + 1 })
-              }
-              reg.lastIndex = 0
-            }
+            reg.lastIndex = 0
           }
+        }
 
-          return {
-            file,
-            regexLibs: [...regexLibsMap] as regexLibs[],
-            npmLibs: [...npmLibsMap] as npmLibs[],
-          }
-        }),
-      )
-
-      return found.reduce(
-        (acc, o) => (o === false ? acc : { ...acc, [o.file.path]: o }),
-        {} as foundMentionsMap,
-      )
+        found[file.path] = {
+          file,
+          regexLibs: [...regexLibsMap] as regexLibs[],
+          npmLibs: [...npmLibsMap] as npmLibs[],
+        }
+      }
+      return found
     }
 
     if (app.type === APP_TYPES.reactNative) {
