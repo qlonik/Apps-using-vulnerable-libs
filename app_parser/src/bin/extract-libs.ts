fe4101011d4d2b832ffd15f60d57ee467b9c8f1e
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-06-26 18:01:40 -0400

    feat(bin/extract-lib): extract libs from dump folder

---
+++ app_parser/src/bin/extract-libs.ts
@@ -0,0 +1,45 @@
+import { readdir } from 'fs-extra'
+import { partition, shuffle, filter } from 'lodash/fp'
+import { join } from 'path'
+import { resolveAllOrInParallel } from '../utils'
+import { poolFactory } from '../utils/worker'
+import { allMessages, MainFn, WORKER_FILENAME } from './_all.types'
+
+const DUMP_PATH = '../data/snyk/dump'
+const LIBS_PATH = '../data/snyk/sample_libs'
+
+let terminating = false
+export const main: MainFn = async function main(log) {
+  const pool = poolFactory<allMessages>(join(__dirname, WORKER_FILENAME))
+  const filenames = shuffle(
+    filter((s) => !s.startsWith('_') && s.endsWith('.tgz'), await readdir(DUMP_PATH)),
+  )
+
+  log.info('started processing %o files', filenames.length)
+  const results = await resolveAllOrInParallel(
+    filenames.map((filename) => async () => {
+      if (terminating) {
+        return { done: false, filename }
+      }
+
+      try {
+        return {
+          done: await pool.exec('extract-lib-from-dump', [
+            { filename, libsPath: LIBS_PATH, dumpPath: DUMP_PATH },
+          ]),
+          filename,
+        }
+      } catch (err) {
+        log.error({ err }, 'error from child')
+        return { done: false, filename }
+      }
+    }),
+  )
+
+  const [s, f] = partition((done) => done, results)
+
+  log.info('successful: %o', s.length)
+  log.info({ failed: f }, 'failed: %o', f.length)
+
+  await pool.terminate()
+}
