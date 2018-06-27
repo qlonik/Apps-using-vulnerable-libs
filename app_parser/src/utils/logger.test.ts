8c39632debf526368b244f869b25ac13089bf9de
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-06-27 00:29:28 -0400

    test(utils/logger): stub pino logger with sinon spies

--- app_parser/src/utils/logger.test.ts
+++ app_parser/src/utils/logger.test.ts
@@ -1,30 +1,39 @@
 import { test } from 'ava'
-import pino from 'pino'
-import { Writable } from 'stream'
-import { assert, log } from './logger'
-
-class DevNull extends Writable {
-  public constructor() {
-    super({})
-  }
-  public _write(chunk: any, enc: any, cb: Function) {
-    setImmediate(cb)
-  }
-}
+import { spy } from 'sinon'
+import { assert } from './logger'
 
 test('assert', t => {
-  t.true(assert(true))
-  t.true(assert(true, log))
-  t.true(assert(true, log, 'msg'))
-  t.true(assert(true, undefined, 'msg'))
-  t.true(assert(true, log, 'msg', 'error'))
-  t.true(assert(true, undefined, 'msg', 'error'))
-  t.true(assert(true, log, undefined, 'error'))
-  t.true(assert(true, undefined, undefined, 'error'))
+  const log = {
+    error: spy(),
+    fatal: spy(),
+  }
 
-  t.throws(() => assert(false, pino(new DevNull())), {
-    message: 'assertion error',
-    instanceOf: Error,
-  })
-  t.throws(() => assert(false, pino(new DevNull()), 'msg'), { message: 'msg', instanceOf: Error })
+  t.true(assert(true))
+  t.true(assert(true, log as any))
+  t.true(assert(true, log as any, 'msg'))
+  t.true(assert(true, undefined, 'msg'))
+  t.true(assert(true, log as any, 'msg', 'error'))
+  t.true(assert(true, undefined, 'msg', 'error'))
+  t.true(assert(true, log as any, undefined, 'error'))
+  t.true(assert(true, undefined, undefined, 'error'))
+  t.true(log.error.notCalled)
+  t.true(log.fatal.notCalled)
+
+  log.error.resetHistory()
+  log.fatal.resetHistory()
+  t.throws(() => assert(false, log as any), { message: 'assertion error', instanceOf: Error })
+  t.true(log.error.calledOnce)
+  t.true(log.fatal.notCalled)
+
+  log.error.resetHistory()
+  log.fatal.resetHistory()
+  t.throws(() => assert(false, log as any, 'msg'), { message: 'msg', instanceOf: Error })
+  t.true(log.error.calledOnce)
+  t.true(log.fatal.notCalled)
+
+  log.error.resetHistory()
+  log.fatal.resetHistory()
+  t.throws(() => assert(false, log as any, 'msg', 'fatal'), { message: 'msg', instanceOf: Error })
+  t.true(log.error.notCalled)
+  t.true(log.fatal.calledOnce)
 })
