de5a5a80388a7ae3224e7e43283e0f9e1900d265
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-03 01:47:42 -0400

    feat(types/changes-stream): improve typings for 'changes-stream'

--- app_parser/src/types/changes-stream/index.d.ts
+++ app_parser/src/types/changes-stream/index.d.ts
@@ -1,2 +1,24 @@
-declare const x: any
-export = x
+import { Readable } from 'stream'
+import { Agent } from 'http'
+
+interface Options {
+  db: string
+  feed?: 'continuous' | 'longpoll'
+  filter?: string | string[] | (() => boolean)
+  inactivity_ms?: number
+  timeout?: number
+  requestTimeout?: number
+  agent?: Agent
+  since?: number | 'now'
+  heartbeat?: number
+  style?: string
+  include_docs?: boolean
+  query_params?: { [name: string]: string }
+  use_post?: boolean
+}
+
+declare class ChangesStream extends Readable {
+  public constructor(options: string | Options)
+}
+
+export = ChangesStream
