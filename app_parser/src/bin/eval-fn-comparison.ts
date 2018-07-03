589bbde321cd5471f3df2757b8fa392f7259a759
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-02 20:23:13 -0400

    feat(bin): eval lib and fn comparison data
    These two binary files evaluate the random functions extracted from apps. These
    two files show whether the comparison between functions and comparison between
    libraries computes expected values.

---
+++ app_parser/src/bin/eval-fn-comparison.ts
@@ -0,0 +1,261 @@
+import { curry, head, isEqual, once, partition } from 'lodash/fp'
+import { Logger } from 'pino'
+import { FunctionSignature } from '../extractStructure'
+import {
+  data,
+  GoneMisMatchedFn,
+  MisMatchedReason as r,
+  ModMisMatchedFn,
+  noMisMatched,
+} from '../manual-reports/rnd-fns-eval'
+import {
+  analysisFile,
+  APP_TYPES,
+  appDesc,
+  getAnalysedData,
+  getCordovaAnalysisFiles,
+  getReactNativeAnalysisFiles,
+} from '../parseApps'
+import { getLibNameVersionSigContents, libNameVersionSigFile } from '../parseLibraries'
+import { jaccardLike } from '../similarityIndex/set'
+import { assertNever, loAsync } from '../utils'
+import { assert } from '../utils/logger'
+import { MainFn, TerminateFn } from './_all.types'
+
+const APPS_PATH = '../data/sample_apps'
+const LIBS_PATH = '../data/sample_libs'
+
+type appSpec = {
+  app: appDesc
+  file: analysisFile
+  signature: FunctionSignature
+}
+/**
+ * @throws when app does not pass common checks and when loaded signature is
+ *   not expected
+ * @param spec app signature specifier
+ * @param _log logger instance
+ */
+const checkAppFnSig = async (spec: appSpec, _log: Logger): Promise<FunctionSignature> => {
+  const fn =
+    spec.app.type === APP_TYPES.cordova
+      ? getCordovaAnalysisFiles
+      : spec.app.type === APP_TYPES.reactNative
+        ? getReactNativeAnalysisFiles
+        : assertNever(spec.app.type)
+  const loadedFiles: analysisFile[] = await fn(APPS_PATH, spec.app)
+  assert(loadedFiles.length !== 0, _log, 'this app has no analysis files')
+
+  const neededFile: analysisFile = assert(
+    loadedFiles.find(({ path }) => path === spec.file.path),
+    _log,
+    'this app does not have specified file',
+  )
+  const data = await getAnalysedData(APPS_PATH, spec.app, neededFile)
+  const signature = assert(data.signature, _log, 'this app has no signature for this file')
+  const fnSig = assert(
+    signature.functionSignature[spec.signature.index],
+    _log,
+    'fn sig at specified index does not exist',
+  )
+  assert(
+    isEqual(fnSig, spec.signature),
+    _log.child({ fnSig: { index: fnSig.index } }),
+    'specified fn sig is not the same as loaded fn sig',
+  )
+  return spec.signature
+}
+
+const loadLibSig = curry(
+  /**
+   * @throws when loaded 0 or more than 1 signature.
+   * @param _log
+   * @param spec
+   */
+  async (_log: Logger, spec: libNameVersionSigFile): Promise<FunctionSignature[]> => {
+    const contents = await getLibNameVersionSigContents(
+      LIBS_PATH,
+      spec.name,
+      spec.version,
+      spec.file,
+    )
+    assert(
+      contents.length === 1,
+      _log.child({ numLoadedSigs: contents.length }),
+      'not a single signature loaded',
+    )
+
+    const libSig = assert(head(contents), _log, 'no lib signature')
+    return libSig.signature.functionSignature
+  },
+)
+
+type libSpec = libNameVersionSigFile & { index: number }
+const loadLibFnSig = curry(
+  /**
+   * @throws when signature at given index does not exist, and when signature
+   *   at given index does not have expected index.
+   * @param log logger instance
+   * @param spec function signature specifier
+   */
+  async (log: Logger, spec: libSpec): Promise<FunctionSignature> => {
+    const _log = log.child({ 'lib-spec': spec })
+    const libFnSig = await loadLibSig(_log, spec)
+    const fnSig = assert(libFnSig[spec.index], _log, 'fn sig at specified index does not exist')
+    assert(
+      fnSig.index === spec.index,
+      _log.child({ fnSig: { index: fnSig.index } }),
+      'fn sig at specified index has unexpected index',
+    )
+    return fnSig
+  },
+)
+
+const noopMap = () => async (x: any): Promise<typeof x> => x
+const jl = (t: FunctionSignature) => (sig: FunctionSignature) =>
+  jaccardLike(t.fnStatementTokens, sig.fnStatementTokens)
+
+const targetSigCheckerFactory =
+  /**
+   * Function computing indexValues between target function and array of passed
+   * functions
+   * @param t target function signature
+   * @param _log logger instance
+   */
+  (t: FunctionSignature, _log: Logger) =>
+    /**
+     *
+     * @param fn mapping function
+     */
+    <T = libSpec>(fn: (log: Logger) => (x: T) => Promise<FunctionSignature>) =>
+      /**
+       * Function returns pair of numbered index value arrays. First array have
+       * all index values equal to 1, and second array contains all other.
+       * @param c array of libSpecs to check
+       */
+      async (c: T[]) => {
+        const indValPs = c
+          .map(fn(_log))
+          .map(loAsync(jl(t)))
+          .map(async (x, _i) => ({ ...(await x), _i }))
+        return partition((x) => x.val === 1, await Promise.all(indValPs))
+      }
+
+export const main: MainFn = async function main(log) {
+  const totals = await data.concat(noMisMatched).reduce(
+    async (acc, fn) => {
+      const _log = log.child({
+        'app-spec': {
+          ...fn.app,
+          path: fn.file.path,
+          index: fn.signature.index,
+        },
+      })
+
+      const targetSig = await checkAppFnSig(fn, _log)
+      const tSigCh = targetSigCheckerFactory(targetSig, _log)
+      const tSigChFromLibSpec = tSigCh(loadLibFnSig)
+      const tSigChFromLib = tSigCh(noopMap)
+
+      const [targetVersions, expMatch] = partition((o) => o.targetVersion, fn.matchedFns)
+      const [expMisMatch, noFn] = partition(
+        (o) => o.reason === r.mod || o.reason === r.min,
+        fn.misMatchedFns,
+      ) as [ModMisMatchedFn[], GoneMisMatchedFn[]]
+
+      const [
+        [tvOnes, tvOther],
+        [emOnes, emOther],
+        [emmOnes, emmOther],
+        noFnRatings,
+      ] = await Promise.all([
+        tSigChFromLibSpec(targetVersions),
+        tSigChFromLibSpec(expMatch),
+        tSigChFromLibSpec(expMisMatch),
+        Promise.all(
+          noFn.map(async (libSigSpec) => {
+            const libLog = _log.child({ 'lib-spec': libSigSpec })
+            const [nfOnes, nfOther] = await tSigChFromLib(await loadLibSig(libLog, libSigSpec))
+            if (nfOnes.length !== 0) {
+              libLog.error(
+                { 'ones-i': nfOnes.map((o) => o._i) },
+                'some unexpected non-existent fn matched with one',
+              )
+            }
+            return [nfOnes, nfOther]
+          }),
+        ),
+      ])
+
+      if (tvOther.length !== 0) {
+        _log.error(
+          { 'not-ones-i': tvOther.map((o) => o._i) },
+          'not all target versions have 100% match',
+        )
+      }
+      if (emOther.length !== 0) {
+        _log.error(
+          { 'not-ones-i': emOther.map((o) => o._i) },
+          'not all expected matched have 100% match',
+        )
+      }
+      if (emmOnes.length !== 0) {
+        _log.error(
+          { 'ones-i': emmOnes.map((o) => o._i) },
+          'some expected mismatches have 100% match',
+        )
+      }
+
+      const noFnTot = noFnRatings.reduce(
+        (acc, [nfOnes, nfOther]) => ({
+          num: acc.num + nfOther.length,
+          den: acc.den + nfOnes.length + nfOther.length,
+        }),
+        { num: 0, den: 0 },
+      )
+
+      const a = await acc
+      return {
+        tot: a.tot + 1,
+        tv: {
+          num: a.tv.num + tvOnes.length,
+          den: a.tv.den + tvOnes.length + tvOther.length,
+        },
+        em: {
+          num: a.em.num + emOnes.length,
+          den: a.em.den + emOnes.length + emOther.length,
+        },
+        emm: {
+          num: a.emm.num + emmOther.length,
+          den: a.emm.den + emmOnes.length + emmOther.length,
+        },
+        noFn: {
+          num: a.noFn.num + noFnTot.num,
+          den: a.noFn.den + noFnTot.den,
+        },
+      }
+    },
+    Promise.resolve({
+      tot: 0,
+      tv: { num: 0, den: 0 },
+      em: { num: 0, den: 0 },
+      emm: { num: 0, den: 0 },
+      noFn: { num: 0, den: 0 },
+    }),
+  )
+
+  log.info(
+    {
+      totals: {
+        tot: totals.tot,
+        tv: { val: totals.tv.num / totals.tv.den, ...totals.tv },
+        em: { val: totals.em.num / totals.em.den, ...totals.em },
+        emm: { val: totals.emm.num / totals.emm.den, ...totals.emm },
+        noFn: { val: totals.noFn.num / totals.noFn.den, ...totals.noFn },
+      },
+    },
+    "Expected results for function matching. All 'vals' should be equal to 1",
+  )
+}
+
+export const terminate: TerminateFn = once(function terminate() {})
---
+++ app_parser/src/bin/eval-lib-comparison.ts
@@ -0,0 +1,119 @@
+import { stripIndent } from 'common-tags'
+import { isEqual, once, uniqWith } from 'lodash/fp'
+import head from 'lodash/fp/head'
+import { Logger } from 'pino'
+import { signatureNew, signatureWithComments } from '../extractStructure'
+import { data, noMisMatched } from '../manual-reports/rnd-fns-eval'
+import {
+  analysisFile,
+  APP_TYPES,
+  appDesc,
+  getAnalysedData,
+  getCordovaAnalysisFiles,
+  getReactNativeAnalysisFiles,
+} from '../parseApps'
+import { getLibNameVersionSigContents, libNameVersionSigFile } from '../parseLibraries'
+import { librarySimilarityByFunctionStatementTokens_v6 } from '../similarityIndex/similarity-methods'
+import { assertNever } from '../utils'
+import { assert } from '../utils/logger'
+import { MainFn, TerminateFn } from './_all.types'
+
+const APPS_PATH = '../data/sample_apps'
+const LIBS_PATH = '../data/sample_libs'
+
+type appSpec = { app: appDesc; file: analysisFile }
+const loadAppSig = async (_log: Logger, spec: appSpec): Promise<signatureWithComments> => {
+  const fn =
+    spec.app.type === APP_TYPES.cordova
+      ? getCordovaAnalysisFiles
+      : spec.app.type === APP_TYPES.reactNative
+        ? getReactNativeAnalysisFiles
+        : assertNever(spec.app.type)
+  const loadedFiles: analysisFile[] = await fn(APPS_PATH, spec.app)
+  assert(loadedFiles.length !== 0, _log, 'this app has no analysis files')
+
+  const neededFile: analysisFile = assert(
+    loadedFiles.find(({ path }) => path === spec.file.path),
+    _log,
+    'this app does not have specified file',
+  )
+  const data = await getAnalysedData(APPS_PATH, spec.app, neededFile)
+  return assert(data.signature, _log, 'this app has no signature for this file')
+}
+const loadLibSig = async (_log: Logger, spec: libNameVersionSigFile): Promise<signatureNew> => {
+  const contents = await getLibNameVersionSigContents(LIBS_PATH, spec.name, spec.version, spec.file)
+  assert(
+    contents.length === 1,
+    _log.child({ numLoadedSigs: contents.length }),
+    'not a single signature loaded',
+  )
+
+  const libSig = assert(head(contents), _log, 'no lib signature')
+  return libSig.signature
+}
+
+type AppFileTargetSpec = {
+  app: appDesc
+  file: analysisFile
+  target: libNameVersionSigFile
+}
+export const main: MainFn = async function main(log) {
+  const appFileTargetSpecs = data
+    .concat(noMisMatched)
+    .map(({ app, file, matchedFns }) => {
+      const targets = matchedFns
+        .filter((o) => o.targetVersion)
+        .map(({ name, version, file }): libNameVersionSigFile => ({ name, version, file }))
+      return targets.length === 0 ? null : { app, file, targets }
+    })
+    .filter((i): i is NonNullable<typeof i> => i !== null && i !== undefined)
+    .reduce(
+      (acc, spec) =>
+        acc.concat(spec.targets.map((target) => ({ app: spec.app, file: spec.file, target }))),
+      [] as AppFileTargetSpec[],
+    )
+  const uniqAppFileTargetSpecs = uniqWith(isEqual, appFileTargetSpecs)
+  const totals = await uniqAppFileTargetSpecs.reduce(
+    async (acc, spec) => {
+      const _log = log.child({
+        'app-spec': { ...spec.app, path: spec.file },
+        'lib-spec': spec.target,
+      })
+
+      const [appSig, libSig] = await Promise.all([
+        loadAppSig(_log, spec),
+        loadLibSig(_log, spec.target),
+      ])
+
+      const { similarity } = librarySimilarityByFunctionStatementTokens_v6(appSig, libSig)
+
+      const t = await acc
+      return {
+        tot: t.tot + 1,
+        emptyLib: t.emptyLib + (libSig.functionSignature.length === 0 ? 1 : 0),
+        exactMatch: t.exactMatch + (similarity.val === 1 ? 1 : 0),
+      }
+    },
+    Promise.resolve({
+      tot: 0,
+      emptyLib: 0,
+      exactMatch: 0,
+    }),
+  )
+
+  log.info(
+    {
+      totals: {
+        tot: totals.tot,
+        emptyLib: totals.emptyLib,
+        exactMatch: totals.exactMatch,
+      },
+    },
+    stripIndent`
+      Expected results for lib matching.
+      emptyLib should be 0, exactMatch should be equal to tot
+    `,
+  )
+}
+
+export const terminate: TerminateFn = once(function terminate() {})
