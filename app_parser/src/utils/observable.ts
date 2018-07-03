582ef15198724815d5d2f95a98edfd504843d356
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-03 01:48:34 -0400

    feat(utils/observable): add fn to convert Stream to Observable
    This function is imported from github repository rx-stream. Source link:
    https://github.com/Jason3S/rx-stream/blob/ce5e7e0b3e1872aa2f458e30faa5f980264cba9c/src/streamToRx.ts#L4

--- app_parser/src/utils/observable.ts
+++ app_parser/src/utils/observable.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from 'events'
+import { Observable as RxOb } from 'rxjs'
 import Observable from 'zen-observable'
 
 export const observableFromEventEmitter = (ee: EventEmitter, ev: string) => {
@@ -10,3 +11,20 @@ export const observableFromEventEmitter = (ee: EventEmitter, ev: string) => {
     return () => ee.removeListener(ev, onMsgCb)
   })
 }
+
+export const streamToRx = <T>(stream: NodeJS.ReadableStream) =>
+  new RxOb<T>((subscriber) => {
+    const endHandler = () => subscriber.complete()
+    const errorHandler = (e: Error) => subscriber.error(e)
+    const dataHandler = (data: T) => subscriber.next(data)
+
+    stream.addListener('end', endHandler)
+    stream.addListener('error', errorHandler)
+    stream.addListener('data', dataHandler)
+
+    return () => {
+      stream.removeListener('end', endHandler)
+      stream.removeListener('error', errorHandler)
+      stream.removeListener('data', dataHandler)
+    }
+  })
