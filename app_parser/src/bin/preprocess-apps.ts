321325f172265866937d3be97f416d67e0b84584
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-11 22:27:22 -0400

    feat(bin/preprocess-apps): remove chunking, run all at the same time

--- app_parser/src/bin/preprocess-apps.ts
+++ app_parser/src/bin/preprocess-apps.ts
@@ -1,12 +1,10 @@
 import { pathExists, readJSON } from 'fs-extra'
 import { differenceWith, isEqual, partition, once } from 'lodash'
 import shuffle from 'lodash/fp/shuffle'
-import take from 'lodash/fp/take'
 import { join } from 'path'
 import { Pool } from 'workerpool'
 import { appDesc, getApps } from '../parseApps'
 import { FINISHED_PREPROCESSING_FILE } from '../parseApps/constants'
-import { resolveAllOrInParallel } from '../utils'
 import { myWriteJSON } from '../utils/files'
 import { log } from '../utils/logger'
 import { poolFactory } from '../utils/worker'
@@ -16,7 +14,6 @@ import { WORKER_FILENAME, allMessages, MainFn, TerminateFn } from './_all.types'
 const APP_PATH = '../data/sample_apps'
 const FIN_APPS_PATH = join(APP_PATH, FINISHED_PREPROCESSING_FILE)
 const LIB_PATH = '../data/sample_libs'
-const ANALYSE_NUMBER = 1500
 
 let pool: Pool<allMessages>
 let terminating = false
@@ -36,7 +33,7 @@ export const main: MainFn = async function main() {
   }
 
   const filtered = differenceWith(apps, FIN_APPS, isEqual)
-  const todo = take(ANALYSE_NUMBER, shuffle(filtered))
+  const todo = shuffle(filtered)
   log.info(
     'apps: (all=%o)-(fin=%o)=(todo=%o/%o)',
     apps.length,
@@ -49,7 +46,7 @@ export const main: MainFn = async function main() {
   log.info({ stats: pool.stats() }, 'pool: min=%o, max=%o', pool.minWorkers, pool.maxWorkers)
 
   // analyse chosen apps promises
-  const appsPromises = todo.map((app) => async () => {
+  const appsPromises = todo.map(async (app) => {
     if (terminating) {
       return { done: false, ...app }
     }
@@ -60,26 +57,16 @@ export const main: MainFn = async function main() {
   })
 
   log.info('started preprocessing')
-  const results = await resolveAllOrInParallel(appsPromises, {
-    chunkLimit: pool.maxWorkers + 1,
-    chunkSize: Math.floor(1.5 * pool.maxWorkers),
-    chunkTapFn: async (apps) => {
-      const finished = apps
-        .filter(({ done }) => done)
-        .map(({ type, section, app }) => ({ type, section, app }))
-
-      if (finished.length > 0) {
-        FIN_APPS = FIN_APPS.concat(finished)
-      }
-    },
-  })
+  const results = await Promise.all(appsPromises)
   if (terminating) {
     log.info('terminated preprocessing')
   } else {
     log.info('finished preprocessing')
   }
 
-  const [done, notDone] = partition(results, ({ done }) => done)
+  const [done, notDone] = partition(results, ({ done }) => done).map((apps) =>
+    apps.map(({ type, section, app }) => ({ type, section, app })),
+  )
   const doneLength = done.length
   const notDoneLength = notDone.length
 
@@ -91,7 +78,7 @@ export const main: MainFn = async function main() {
     filtered.length,
   )
 
-  await myWriteJSON({ content: FIN_APPS, file: FIN_APPS_PATH })
+  await myWriteJSON({ content: FIN_APPS.concat(done), file: FIN_APPS_PATH })
   log.info('updated FIN_APPS')
 
   await pool.terminate()
