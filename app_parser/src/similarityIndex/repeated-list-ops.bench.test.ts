89ebd6838d67f3a20890dfeffd84d70a2253e54c
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-24 00:24:59 -0400

    test(sim:repeated-list-ops): add benchmark for repeated list operations

---
+++ app_parser/src/similarityIndex/repeated-list-ops.bench.test.ts
@@ -0,0 +1,41 @@
+import { test } from 'ava'
+import suite from 'chuhai'
+import arb from 'jsverify'
+import { isEqual } from 'lodash/fp'
+import { arraysPair, repeatingArr } from '../_helpers/arbitraries'
+import { check } from '../_helpers/property-test'
+import { Ops, repeatedOps, repeatedOpsFp } from './repeated-list-ops'
+
+const AB = arraysPair(arb.integer, repeatingArr)
+
+test.serial(
+  'repeatedOps vs repeatedOpsFp',
+  check({ tests: 2 }, AB, async (t, [a, b]) => {
+    /* eslint-disable no-console */
+    const origConsoleLog = console.log
+    console.log = t.log
+
+    await suite('repeatedOps vs repeatedOpsFp', s => {
+      let bench: Ops<number> | null = null
+
+      const repOps = repeatedOps(isEqual)
+      const repOpsFp = repeatedOpsFp(isEqual)
+
+      s.cycle(() => {
+        t.not(null, bench)
+
+        bench = null
+      })
+
+      s.bench('for-loops', () => {
+        bench = repOps(a, b)
+      })
+      s.bench('reduce', () => {
+        bench = repOpsFp(a, b)
+      })
+    })
+
+    console.log = origConsoleLog
+    /* eslint-enable no-console */
+  }),
+)
