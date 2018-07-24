b8c322eb221bb28eff94729d68fc19126eab6b84
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-23 23:11:17 -0400

    chore(package.json): add chuhai benchmarking tool

---
+++ app_parser/src/types/chuhai/index.d.ts
@@ -0,0 +1,27 @@
+interface Opts {
+  async: boolean
+  defer: boolean
+  delay: number
+  id: string
+  initCount: number
+  maxTime: number
+  minSamples: number
+  minTime: number
+  name: string
+  onAbort: () => any
+  onComplete: () => any
+  onCycle: () => any
+  onError: () => any
+  onReset: () => any
+  onStart: () => any
+}
+
+declare interface ChuhaiBench {
+  set<T extends keyof Opts>(key: T, value: Opts[T]): void
+
+  cycle(impl: () => any): void
+  burn(title: string, impl: () => any): void
+  bench(title: string, impl: () => any): void
+}
+
+export default function chuhaiFn(title: string, impl: (s: ChuhaiBench) => any): Promise<any>
