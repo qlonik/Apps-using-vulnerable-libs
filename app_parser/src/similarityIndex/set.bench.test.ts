b5fe47f0847ebcd8532a3358903e44d802a9bdf7
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-07-25 04:44:45 -0400

    test(set.bench): rewrite bench to compare all indexValue returning fns

--- app_parser/src/similarityIndex/set.bench.test.ts
+++ app_parser/src/similarityIndex/set.bench.test.ts
@@ -1,50 +1,98 @@
 import { test } from 'ava'
-import arb from 'jsverify'
 import suite from 'chuhai'
+import arb from 'jsverify'
+import { identity, shuffle } from 'lodash/fp'
 import { arraysPair, repeatingArr } from '../_helpers/arbitraries'
 import { check } from '../_helpers/property-test'
-import { indexValue, jaccardLike, jaccardLikeWithMapping, libPortion } from './set'
+import { numComp, strComp } from './bin-search'
+import {
+  indexValue,
+  jaccardLike,
+  jaccardLikeNumbers,
+  jaccardLikeStrings,
+  jaccardLikeWithMapping,
+  libPortion,
+  libPortionIndexes,
+} from './set'
 
-const AB = arraysPair(arb.integer(0, 1000), repeatingArr)
-
-test.serial(
-  'jaccardLike vs jaccardLikeWithMapping vs libPortion',
-  check({ tests: 3, size: 500, rngState: '010123456789abcdef' }, AB, async (t, [a, b]) => {
-    /* eslint-disable no-console */
-    const origConsoleLog = console.log
-    console.log = t.log
-
-    await suite('jaccardLike vs jaccardLikeWithMapping vs libPortion', s => {
-      s.set('maxTime', 1)
-      s.set('minSamples', 10)
-
-      let bench: indexValue | null = null
-
-      s.cycle(() => {
-        if (bench === null) {
-          return t.fail()
-        }
-
-        t.is('object', typeof bench)
-        t.is('number', typeof bench.val)
-        t.is('number', typeof bench.num)
-        t.is('number', typeof bench.den)
-
-        bench = null
-      })
-
-      s.bench('jaccardLikeWithMapping', () => {
-        bench = jaccardLikeWithMapping(a, b).similarity
-      })
-      s.bench('jaccardLike', () => {
-        bench = jaccardLike(a, b)
-      })
-      s.bench('libPortion', () => {
-        bench = libPortion(a, b)
-      })
-    })
-
-    console.log = origConsoleLog
-    /* eslint-enable no-console */
-  }),
+const sAB = arraysPair(arb.nestring, repeatingArr).smap<[string[], string[]]>(
+  ([a, b]) => [a.sort(strComp), b.sort(strComp)],
+  identity,
 )
+const nAB = arraysPair(arb.integer(0, 1000), repeatingArr).smap<[number[], number[]]>(
+  ([a, b]) => [a.sort(numComp), b.sort(numComp)],
+  identity,
+)
+
+type testTuple<T> = [string, arb.Arbitrary<[T[], T[]]>, (a: T[], b: T[]) => indexValue]
+const tests: testTuple<any>[] = [
+  ['Strings', sAB, jaccardLikeStrings] as testTuple<string>,
+  ['Numbers', nAB, jaccardLikeNumbers] as testTuple<number>,
+]
+
+for (let [name, arbitrary, fn] of tests) {
+  test.serial(
+    `${name}: all fns producing indexValue`,
+    check({ tests: 1, size: 500 }, arbitrary, async (t, [a, b]) => {
+      /* eslint-disable no-console */
+      const origConsoleLog = console.log
+      console.log = t.log
+
+      const aSh = shuffle(a)
+      const bSh = shuffle(b)
+
+      await suite(t.title, s => {
+        s.set('maxTime', 1)
+        s.set('minSamples', 10)
+
+        let bench: indexValue | null = null
+
+        s.cycle(() => {
+          if (bench !== null) {
+            t.deepEqual(jaccardLike(a, b), bench)
+          } else if (bench === null) {
+            t.pass()
+          }
+          bench = null
+        })
+
+        s.bench('jaccardLike shuffled', () => {
+          bench = jaccardLike(aSh, bSh)
+        })
+        s.bench('jaccardLike sorted', () => {
+          bench = jaccardLike(a, b)
+        })
+        s.bench('jaccardLikeWithMapping shuffled', () => {
+          bench = jaccardLikeWithMapping(aSh, bSh).similarity
+        })
+        s.bench('jaccardLikeWithMapping sorted', () => {
+          bench = jaccardLikeWithMapping(a, b).similarity
+        })
+        s.bench('libPortion shuffled', () => {
+          libPortion(aSh, bSh)
+        })
+        s.bench('libPortion sorted', () => {
+          libPortion(a, b)
+        })
+        s.bench(`jaccardLike${name} shuffled`, () => {
+          bench = fn(aSh, bSh)
+        })
+        s.bench(`jaccardLike${name} sorted`, () => {
+          bench = fn(a, b)
+        })
+
+        if (name === 'Numbers') {
+          s.bench('libPortionIndexes shuffled', () => {
+            libPortionIndexes(aSh, bSh)
+          })
+          s.bench('libPortionIndexes sorted', () => {
+            libPortionIndexes(a, b)
+          })
+        }
+      })
+
+      console.log = origConsoleLog
+      /* eslint-enable no-console */
+    }),
+  )
+}
