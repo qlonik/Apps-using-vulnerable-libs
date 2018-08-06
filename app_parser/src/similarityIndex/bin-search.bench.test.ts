68ab4630e12214814ef300f6ddd5e1215f73252a
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-08-06 17:32:31 -0400

    test(bin-search.bench): split string and number tests

--- app_parser/src/similarityIndex/bin-search.bench.test.ts
+++ app_parser/src/similarityIndex/bin-search.bench.test.ts
@@ -1,61 +1,61 @@
-import { test } from 'ava'
+import { test, ExecutionContext } from 'ava'
 import suite from 'chuhai'
 import arb from 'jsverify'
 import { sortedIndexOf } from 'lodash/fp'
 import { check } from '../_helpers/property-test'
 import { binarySearch, numComp, strComp } from './bin-search'
 
-test(
-  'binSearch vs sortedIndexOf vs findIndex',
-  check(
-    { tests: 1 },
-    arb.nearray(arb.number).smap(ar => ar.sort(numComp), ar => ar),
-    arb.number,
-    arb.nearray(arb.string).smap(ar => ar.sort(strComp), ar => ar),
-    arb.string,
-    async (t, nArr, nEl, sArr, sEl) => {
-      /* eslint-disable no-console */
-      const origConsoleLog = console.log
-      console.log = t.log
+const arrAndEl = <T extends string | number>(
+  comp: (a: T, b: T) => number,
+  el: arb.Arbitrary<T>,
+): [typeof comp, arb.Arbitrary<T>, arb.Arbitrary<T[]>] => [
+  comp,
+  el,
+  arb.nearray(el).smap(ar => ar.sort(comp), ar => ar),
+]
 
-      await suite(t.title, s => {
-        s.set('maxTime', 2)
-        s.set('minSamples', 10)
+const tests = [
+  ['numbers', ...arrAndEl(numComp, arb.number)],
+  ['strings', ...arrAndEl(strComp, arb.string)],
+]
 
-        const nFound = nArr.findIndex(v => v === nEl)
-        const sFound = sArr.findIndex(v => v === sEl)
-        let nBench: number | null = null
-        let sBench: number | null = null
+for (let [name, comp, someArb, someArbArr] of tests) {
+  test.serial(
+    `${name}: binSearch vs sortedIndexOf vs findIndex`,
+    check(
+      { tests: 1 },
+      someArb,
+      someArbArr,
+      async (t: ExecutionContext, el: number | string, arr: (typeof el)[]) => {
+        /* eslint-disable no-console */
+        const origConsoleLog = console.log
+        console.log = t.log
 
-        s.cycle(() => {
-          t.not(null, nBench || sBench)
-          t.true(nFound === nBench || sFound === sBench)
-          nBench = null
-          sBench = null
+        await suite(t.title, s => {
+          s.set('maxTime', 2)
+          s.set('minSamples', 10)
+
+          let bench: number | null = null
+
+          s.cycle(() => {
+            t.is(arr.findIndex(v => v === el), bench)
+            bench = null
+          })
+
+          s.bench(`${name} binSearch`, () => {
+            bench = binarySearch(arr, el, comp as (a: typeof el, b: typeof el) => number)
+          })
+          s.bench(`${name} sortedIndexOf`, () => {
+            bench = sortedIndexOf(el, arr)
+          })
+          s.bench(`${name} findIndex`, () => {
+            bench = arr.findIndex(v => v === el)
+          })
         })
 
-        s.bench('number binSearch', () => {
-          nBench = binarySearch(nArr, nEl, numComp)
-        })
-        s.bench('string binSearch', () => {
-          sBench = binarySearch(sArr, sEl, strComp)
-        })
-        s.bench('number sortedIndexOf', () => {
-          nBench = sortedIndexOf(nEl, nArr)
-        })
-        s.bench('string sortedIndexOf', () => {
-          sBench = sortedIndexOf(sEl, sArr)
-        })
-        s.bench('number findIndex', () => {
-          nBench = nArr.findIndex(el => el === nEl)
-        })
-        s.bench('string findIndex', () => {
-          sBench = sArr.findIndex(el => el === sEl)
-        })
-      })
-
-      console.log = origConsoleLog
-      /* eslint-enable no-console */
-    },
-  ),
-)
+        console.log = origConsoleLog
+        /* eslint-enable no-console */
+      },
+    ),
+  )
+}
