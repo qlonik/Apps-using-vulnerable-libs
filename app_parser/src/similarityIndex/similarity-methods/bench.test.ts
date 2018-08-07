1e9f41d81d5c92749bc4ba954672d66b62b27aa5
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-08-07 01:14:14 -0400

    test(similarity-methods.bench): merge all matching fn
    This will run one performance run, comparing all different methods, instead of
    two separate.

--- app_parser/src/similarityIndex/similarity-methods/bench.test.ts
+++ app_parser/src/similarityIndex/similarity-methods/bench.test.ts
@@ -1,10 +1,7 @@
 import { test } from 'ava'
 import suite from 'chuhai'
 import { Logger } from 'pino'
-import {
-  arbFunctionSignatureArrPair,
-  arbSignatureWithCommentsPair,
-} from '../../_helpers/arbitraries'
+import { arbSignatureWithCommentsPair } from '../../_helpers/arbitraries'
 import { check } from '../../_helpers/property-test'
 import {
   FunctionSignature,
@@ -24,7 +21,6 @@ import {
   librarySimilarityByFunctionStatementTypes,
   librarySimilarityByLiteralValues,
 } from './index'
-import { SimMapWithConfidence } from './types'
 
 type MatchFn =
   | (<T extends FunctionSignature[] | FunctionSignatures>(l: Logger | undefined, a: T, b: T) => any)
@@ -34,6 +30,12 @@ const tests: [string, MatchFn][] = [
   ['FnNames', librarySimilarityByFunctionNames],
   ['FnNamesAndStTokens', librarySimilarityByFunctionNamesAndStatementTokens],
   ['LitVals', librarySimilarityByLiteralValues],
+  ['FnStTokens_v1', librarySimilarityByFunctionStatementTokens_v1],
+  ['FnStTokens_v2', librarySimilarityByFunctionStatementTokens_v2],
+  ['FnStTokens_v3', librarySimilarityByFunctionStatementTokens_v3],
+  ['FnStTokens_v4', librarySimilarityByFunctionStatementTokens_v4],
+  ['FnStTokens_v5', librarySimilarityByFunctionStatementTokens_v5],
+  ['FnStTokens_v6', librarySimilarityByFunctionStatementTokens_v6],
 ]
 test.serial(
   'matching fn perf',
@@ -59,56 +61,6 @@ test.serial(
         })
 
         for (let [name, fn] of tests) {
-          s.bench(name, () => {
-            val = fn(undefined, a, b)
-          })
-        }
-      })
-
-      console.log = origConsoleLog
-      /* eslint-enable no-console */
-    },
-  ),
-)
-
-type fnStToksMatchFn = <T extends FunctionSignature[] | FunctionSignatures>(
-  l: Logger | undefined,
-  a: T,
-  b: T,
-) => SimMapWithConfidence
-const fnStToksTests: [string, fnStToksMatchFn][] = [
-  ['FnStTokens_v1', librarySimilarityByFunctionStatementTokens_v1],
-  ['FnStTokens_v2', librarySimilarityByFunctionStatementTokens_v2],
-  ['FnStTokens_v3', librarySimilarityByFunctionStatementTokens_v3],
-  ['FnStTokens_v4', librarySimilarityByFunctionStatementTokens_v4],
-  ['FnStTokens_v5', librarySimilarityByFunctionStatementTokens_v5],
-  ['FnStTokens_v6', librarySimilarityByFunctionStatementTokens_v6],
-]
-
-test.serial(
-  'fn-st-toks perf',
-  check(
-    { tests: 1, size: 500, rngState: '010123456789abcdef' },
-    arbFunctionSignatureArrPair,
-    async (t, [a, b]) => {
-      /* eslint-disable no-console */
-      const origConsoleLog = console.log
-      console.log = t.log
-
-      await suite('fn-st-toks perf', s => {
-        s.set('maxTime', 2)
-        s.set('minSamples', 10)
-
-        let val: ReturnType<fnStToksMatchFn> | null = null
-
-        s.cycle(() => {
-          t.not(null, val)
-          t.is('object', typeof val)
-
-          val = null
-        })
-
-        for (let [name, fn] of fnStToksTests) {
           s.bench(name, () => {
             val = fn(undefined, a, b)
           })
