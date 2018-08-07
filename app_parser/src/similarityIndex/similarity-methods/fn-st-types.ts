5e51a0e6ef0d3506e7054acf14387f2e61cf991d
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-08-07 15:50:55 -0400

    feat(sim-methods:fn-st-types): produce similarity and mapping
    Before, this method would only produce similarity, but no mapping. This, change
    brings method used in fnStToks_v1 into this function. Now,
    librarySimilarityByFunctionStatementTypes returns similarity and mapping.

--- app_parser/src/similarityIndex/similarity-methods/fn-st-types.ts
+++ app_parser/src/similarityIndex/similarity-methods/fn-st-types.ts
@@ -1,4 +1,4 @@
-import { clone, head, pullAt } from 'lodash'
+import { clone, head } from 'lodash'
 import { Logger } from 'pino'
 import {
   FunctionSignature,
@@ -6,13 +6,20 @@ import {
   isFunctionSignatures,
 } from '../../extractStructure'
 import logger from '../../utils/logger'
-import { indexValue, jaccardLike } from '../set'
+import { jaccardLike } from '../set'
 import { SortedLimitedList } from '../SortedLimitedList'
-import { nameProb, nameProbIndex, typeErrorMsg } from './types'
+import {
+  DefiniteMap,
+  FunctionSignatureMatched,
+  nameProbIndex,
+  probIndex,
+  SimMapWithConfidence,
+  typeErrorMsg,
+} from './types'
 
 export function librarySimilarityByFunctionStatementTypes<
   T extends FunctionSignature[] | FunctionSignatures
->(log: Logger = logger, unknownS: T, libS: T): indexValue {
+>(log: Logger = logger, unknownS: T, libS: T): SimMapWithConfidence {
   let unknown: FunctionSignature[]
   let lib: FunctionSignature[]
   if (isFunctionSignatures(unknownS) && isFunctionSignatures(libS)) {
@@ -25,31 +32,41 @@ export function librarySimilarityByFunctionStatementTypes<
     throw new TypeError(typeErrorMsg)
   }
 
-  const libCopy = clone(lib)
+  const libCopy = clone(lib) as FunctionSignatureMatched[]
   // remark: first for loop
   const possibleFnNames = unknown.reduce(
-    (acc: nameProb[], { fnStatementTypes: types }: FunctionSignature) => {
+    (acc: nameProbIndex[], { fnStatementTypes: types }) => {
       // remark: second for loop
       const topName = libCopy
-        .reduce((indexes, { name, fnStatementTypes: libTypes }: FunctionSignature, libIndex) => {
-          // remark: threshold can go here
-          // remark: third for loop (inside jaccardLike())
-          return indexes.push({ name, index: libIndex, prob: jaccardLike(types, libTypes) })
+        .reduce((indexes, { name, __matched = false, fnStatementTypes: libTypes }, libIndex) => {
+          return __matched
+            ? indexes
+            : // remark: threshold can go here
+              // remark: third for loop (inside jaccardLike())
+              indexes.push({ name, index: libIndex, prob: jaccardLike(types, libTypes) })
         }, new SortedLimitedList({ predicate: (o: nameProbIndex) => -o.prob.val }))
         .value()
 
       const topMatch = head(topName)
       if (!topMatch || topMatch.prob.val === 0) {
-        const unmatched = { name: '__unmatched__', prob: { val: 1, num: -1, den: -1 } }
+        const unmatched = { name: '__unmatched__', prob: { val: 1, num: -1, den: -1 }, index: -1 }
         return acc.concat(unmatched)
       }
 
       const { name, index, prob } = topMatch
-      pullAt(libCopy, index)
-      return acc.concat({ name, prob })
+      libCopy = libCopy.map((el, i) => (i !== index ? el : { ...el, __matched: true }))
+      return acc.concat({ name, prob, index })
     },
-    [] as nameProb[],
+    [] as nameProbIndex[],
   )
 
-  return jaccardLike(possibleFnNames.map((v) => v.name), lib.map((v) => v.name))
+  const similarity = jaccardLike(possibleFnNames.map((v) => v.name), lib.map((v) => v.name))
+  const mapping = possibleFnNames.reduce(
+    (acc, { index, prob }, unknownIndex) => {
+      return index === -1 ? acc : acc.set(unknownIndex, { index, prob })
+    },
+    new Map() as DefiniteMap<number, probIndex>,
+  )
+
+  return { similarity, mapping }
 }
