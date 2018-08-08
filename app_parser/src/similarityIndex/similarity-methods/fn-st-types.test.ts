4cfcfa41fd1bb5b74ba4b804dad25631e83bf234
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-08-07 23:33:35 -0400

    test(fn-st-types): update tests to reflect SimMap return type

--- app_parser/src/similarityIndex/similarity-methods/fn-st-types.test.ts
+++ app_parser/src/similarityIndex/similarity-methods/fn-st-types.test.ts
@@ -2,7 +2,7 @@ import { test } from 'ava'
 import { cloneDeep } from 'lodash'
 import { arbFunctionSignatureArr, arbFunctionSignatureArrPair } from '../../_helpers/arbitraries'
 import { check } from '../../_helpers/property-test'
-import { EXPECTED_SIMILARITY, LIB_SIG, UNKNOWN_SIG } from './_test-data'
+import { EXPECTED_MAPPING, EXPECTED_SIMILARITY, LIB_SIG, UNKNOWN_SIG } from './_test-data'
 import { librarySimilarityByFunctionStatementTypes } from './fn-st-types'
 
 test('librarySimilarityByFunctionStatementTypes', t => {
@@ -11,7 +11,7 @@ test('librarySimilarityByFunctionStatementTypes', t => {
     cloneDeep(UNKNOWN_SIG),
     cloneDeep(LIB_SIG),
   )
-  t.deepEqual(EXPECTED_SIMILARITY, result)
+  t.deepEqual({ similarity: EXPECTED_SIMILARITY, mapping: EXPECTED_MAPPING }, result)
 })
 
 test(
@@ -29,39 +29,62 @@ test(
 )
 
 test(
-  'commutative',
-  check(arbFunctionSignatureArrPair, (t, [u, l]) => {
+  'commutative similarity',
+  check({ tests: 500 }, arbFunctionSignatureArrPair, (t, [u, l]) => {
     t.deepEqual(
-      librarySimilarityByFunctionStatementTypes(undefined, u, l),
-      librarySimilarityByFunctionStatementTypes(undefined, l, u),
+      librarySimilarityByFunctionStatementTypes(undefined, u, l).similarity,
+      librarySimilarityByFunctionStatementTypes(undefined, l, u).similarity,
     )
   }),
 )
 
 test(
-  'produces 0% match when comparing with empty signature',
+  'produces 0% match when comparing with empty app signature',
   check(arbFunctionSignatureArr, (t, a) => {
-    const similarity = librarySimilarityByFunctionStatementTypes(undefined, a, [])
+    const { similarity, mapping } = librarySimilarityByFunctionStatementTypes(undefined, [], a)
 
     t.is(0, similarity.val)
     t.is(0, similarity.num)
     t.not(0, similarity.den)
+    t.deepEqual(new Map(), mapping)
+  }),
+)
+
+test(
+  'produces 0% match when comparing with empty lib signature',
+  check(arbFunctionSignatureArr, (t, a) => {
+    const { similarity, mapping } = librarySimilarityByFunctionStatementTypes(undefined, a, [])
+
+    t.is(0, similarity.val)
+    t.is(0, similarity.num)
+    t.not(0, similarity.den)
+    t.deepEqual(new Map(), mapping)
   }),
 )
 
 test('produces 100% match when comparing empty signatures', t => {
   t.deepEqual(
-    { val: 1, num: 0, den: 0 },
+    { similarity: { val: 1, num: 0, den: 0 }, mapping: new Map() },
     librarySimilarityByFunctionStatementTypes(undefined, [], []),
   )
 })
 
 test(
   'produces 100% match when comparing same signatures',
-  check(arbFunctionSignatureArr, (t, a) => {
-    const { val, num, den } = librarySimilarityByFunctionStatementTypes(undefined, a, a)
+  check({ tests: 250 }, arbFunctionSignatureArr, (t, a) => {
+    const { similarity: { val, num, den }, mapping } = librarySimilarityByFunctionStatementTypes(
+      undefined,
+      a,
+      a,
+    )
 
     t.is(1, val)
     t.is(num, den)
+    t.is(a.length, mapping.size)
+    for (let [from, { index: to, prob: { val, num, den } }] of mapping) {
+      t.is(from, to)
+      t.is(1, val)
+      t.is(num, den)
+    }
   }),
 )
