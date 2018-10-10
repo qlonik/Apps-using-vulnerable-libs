1351e6b4d4f9ddaf8e16a2a27eca4ce5eb009a02
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-10-10 17:20:26 -0400

    fix(types/fraction.js): add public modifiers on fields

--- app_parser/src/types/fraction.js/index.d.ts
+++ app_parser/src/types/fraction.js/index.d.ts
@@ -2,35 +2,35 @@ type arg0 = number | string | { n: number; d: number } | [number, number]
 type arg1 = number
 
 export declare class Fraction {
-  static REDUCE: boolean
+  public static REDUCE: boolean
 
   public readonly s: -1 | 1
   public readonly n: number
   public readonly d: number
 
-  constructor(a: arg0, b?: arg1)
-  abs(): Fraction
-  neg(): Fraction
-  add(a: arg0, b?: arg1): Fraction
-  sub(a: arg0, b?: arg1): Fraction
-  mul(a: arg0, b?: arg1): Fraction
-  div(a: arg0, b?: arg1): Fraction
-  clone(): Fraction
-  mod(a: arg0, b?: arg1): Fraction
-  gcd(a: arg0, b?: arg1): Fraction
-  lcm(a: arg0, b?: arg1): Fraction
-  ceil(a?: number): Fraction
-  floor(a?: number): Fraction
-  round(a?: number): Fraction
-  inverse(): Fraction
-  pow(a: number): Fraction
-  equals(a: arg0, b?: arg1): Fraction
-  compare(a: arg0, b?: arg1): number
-  simplify(epsilon?: number): Fraction
-  divisible(a: arg0, b?: arg1): Fraction
-  valueOf(): number
-  toFraction(): string
-  toLatex(): string
-  toContinued(): number[]
-  toString(): string
+  public constructor(a: arg0, b?: arg1)
+  public abs(): Fraction
+  public neg(): Fraction
+  public add(a: arg0, b?: arg1): Fraction
+  public sub(a: arg0, b?: arg1): Fraction
+  public mul(a: arg0, b?: arg1): Fraction
+  public div(a: arg0, b?: arg1): Fraction
+  public clone(): Fraction
+  public mod(a: arg0, b?: arg1): Fraction
+  public gcd(a: arg0, b?: arg1): Fraction
+  public lcm(a: arg0, b?: arg1): Fraction
+  public ceil(a?: number): Fraction
+  public floor(a?: number): Fraction
+  public round(a?: number): Fraction
+  public inverse(): Fraction
+  public pow(a: number): Fraction
+  public equals(a: arg0, b?: arg1): Fraction
+  public compare(a: arg0, b?: arg1): number
+  public simplify(epsilon?: number): Fraction
+  public divisible(a: arg0, b?: arg1): Fraction
+  public valueOf(): number
+  public toFraction(): string
+  public toLatex(): string
+  public toContinued(): number[]
+  public toString(): string
 }
