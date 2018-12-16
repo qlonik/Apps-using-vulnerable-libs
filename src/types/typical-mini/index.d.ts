294388503abd6fec10d91aab58ff2dbaf32d642c
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2018-12-15 21:10:03 -0500

    feat(types/typical-mini): export declared types

--- src/types/typical-mini/index.d.ts
+++ src/types/typical-mini/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'typical-mini' {
-  type The<T, V extends T> = V
-  type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
-  type Simplify<T> = Pick<T, keyof T>
+  export type The<T, V extends T> = V
+  export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>
+  export type Simplify<T> = Pick<T, keyof T>
 }
