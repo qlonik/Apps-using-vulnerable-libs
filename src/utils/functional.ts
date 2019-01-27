e0addbbf12056076235be021b4a64eb21ecf53bb
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2019-01-26 19:14:55 -0500

    feat(util:functional): add filterFalsy() and filterNullable()
    Functions to filter array.

--- src/utils/functional.ts
+++ src/utils/functional.ts
@@ -1,5 +1,6 @@
 import { concat, filter, flow, isEqual, join as joinFp, map, replace } from 'lodash/fp'
 import R from 'ramda'
+import { Falsy } from './types'
 
 export const addMissing: <T>(x: T[], y: T[]) => T[] = (x, y) =>
   concat(x, filter((el) => x.findIndex(isEqual(el)) === -1, y))
@@ -28,3 +29,8 @@ export interface FilterFn {
 }
 export const filterFn: FilterFn = R.curry((fn: any, arr: any) => arr.filter(fn))
 
+export const filterFalsy: <T>(x: T[]) => Exclude<T, Falsy>[] = (xs) =>
+  xs.filter((x): x is Exclude<typeof x, Falsy> => !!x)
+
+export const filterNullable: <T>(x: T[]) => NonNullable<T>[] = (xs) =>
+  xs.filter((x): x is NonNullable<typeof x> => x !== null && x !== undefined)
